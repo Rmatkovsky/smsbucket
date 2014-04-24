85086e3ca2a62b26fefeb3025f622697b1eedd6b
Ruslan Matkovsky <ruma@ciklum.com>
2014-04-24 19:20:40 +0300

    added change with the given task #SB-82

--- cloud/main.js
+++ cloud/main.js
@@ -8,26 +8,26 @@ Parse.Cloud.define('addDonation', function(request, response) {
 
     var Requests = Parse.Object.extend("Requests"),
         requests = {};
-//        digitRand = 0000;// Math.floor(Math.random()*10000+1);
 
     if(!!request.params.phoneNumber) {
 
         requests = new Requests();
 
         requests.set({
-            phoneNumber: parseInt(request.params.phoneNumber)
+            phoneNumber: request.params.phoneNumber
         });
 
         var Image = Parse.Object.extend("Pictures"),
             imageQuery = new Parse.Query(Image),
-            photoId = parseInt(request.params.photoId);
+            photoId = parseInt(request.params.photoId),
+            mediacode = null;
 
         imageQuery.get(photoId, {
             success: function(images) {
                 requests.set("donation", ""+request.params.donate);
                 requests.set("donatorName", ""+request.params.senderName);
                 requests.set("email", ""+request.params.senderEmail);
-                requests.set("recipientPhoneNumber", parseInt(request.params.recipientPhoneNumber));
+                requests.set("recipientPhoneNumber", request.params.recipientPhoneNumber);
                 requests.set("imageId",  images);
                 requests.set("text",""+request.params.greetingText);
                 requests.set("used",false);
@@ -35,7 +35,7 @@ Parse.Cloud.define('addDonation', function(request, response) {
                     success: function(saveRequest) {
                         httpRequest({
                                 phoneNumber: request.params.phoneNumber,
-                                text: 'Tryk på linket http://smsbucket.parseapp.com/#/confirm/' + saveRequest.id + ' for at bekræfte din hilsen samt din donation til Røde Kors',
+                                text: 'Tryk på linket http://causeicare.parseapp.com/#/confirm/' + saveRequest.id + ' for at bekræfte din hilsen samt din donation til Røde Kors',
                                 response: response
                             },
                             function() {
@@ -65,21 +65,31 @@ Parse.Cloud.define('addDonation', function(request, response) {
 Parse.Cloud.define('sendDonation', function(request, response) {
 
     var Requests = Parse.Object.extend("Requests"),
-        requestsQuery = new Parse.Query("Requests");
+        requestsQuery = new Parse.Query("Requests"),
+        mediacode = null;
+
+
 
     if(!!request.params.confirmId ) {
             requestsQuery.get(request.params.confirmId,{
                 success: function(item){
+                    switch(item.get('donation')+'') {
+                        case '25': mediacode = 'bouquet25';break;
+                        case '50': mediacode = 'bouquet50';break;
+                        case '75': mediacode = 'bouquet75';break;
+                        case '100': mediacode = 'bouquet100';break;
+                    }
                     httpRequest({
                             phoneNumber: item.get('recipientPhoneNumber'),
-                            text: item.get('donatorName') + ' har sendt dig en hilsen via Cause I Care. Tryk på linket http://smsbucket.parseapp.com/#/sms/' + item.id + ' for at se din hilsen.',
+                            text: item.get('donatorName') + ' har sendt dig en hilsen via Cause I Care. Tryk på linket http://causeicare.parseapp.com/#/sms/' + item.id + ' for at se din hilsen.',
                             response: response
                         },
                         function() {
                             httpRequest({
                                     phoneNumber:item.get('phoneNumber'),
                                     text: 'Tak for dit bidrag på ' + item.get('donation') + ' til Røde Kors. Med venlig hilsen Cause I Care',
-                                    response: response
+                                    response: response,
+                                    mediacode: mediacode
                                 },
                                 function() {
                                     item.set("used",true);
@@ -100,19 +110,24 @@ Parse.Cloud.define('sendDonation', function(request, response) {
 
 });
 function httpRequest(params,callback) {
+    var dParams = {
+        user: cfg.username,
+        password: cfg.password,
+        to: params.phoneNumber,
+//            smsc: 'dk.tdc',
+        price: '0.00DKK',
+        appnr: '1231',
+        text: params.text,
+        mediacode: 'afrika',
+        preferredencoding: 'ISO-8859-1'
+    }
+    if(!!params.mediacode) {
+        dParams.mediacode = params.mediacode;
+        dParams.vat = '0.00';
+    }
     Parse.Cloud.httpRequest({
         url: cfg.unwireUrl,
-        params: {
-            user: cfg.username,
-            password: cfg.password,
-            to: params.phoneNumber,
-            smsc: 'dk.tdc',
-            price: '0.00DKK',
-            appnr: '1231',
-            text: params.text,
-            mediacode: 'afrika',
-            preferredencoding: 'UTF-8'
-        },
+        params: dParams,
         success: function(httpResponse) {
             callback();
 
@@ -126,73 +141,3 @@ function httpRequest(params,callback) {
         }
     });
 }
-
-//Parse.Cloud.define('sendGreeting', function(request, response) {
-//    var Requests = Parse.Object.extend("Requests"),
-//        requestsQuery = new Parse.Query(Requests);
-//
-//    if(!!request.params.confirmId) {
-//        requestsQuery.get(request.params.confirmId,{
-//            success: function(requests) {
-//                    var Image = Parse.Object.extend("Pictures"),
-//                        imageQuery = new Parse.Query(Image),
-//                        photoId = parseInt(request.params.photoId);
-//
-//                    imageQuery.get(photoId, {
-//                        success: function(images) {
-//                            requests.set("donation", request.params.donate);
-//                            requests.set("donatorName", request.params.senderName);
-//                            requests.set("email", request.params.senderEmail);
-//                            requests.set("recipientPhoneNumber", parseInt(request.params.recipientPhoneNumber));
-//                            requests.set("imageId",  images);
-//                            requests.set("text", request.params.greetingText);
-//                            requests.save(null,{
-//                                success: function(saveRequest) {
-//                                    httpRequest({
-//                                        phoneNumber: request.params.recipientPhoneNumber,
-//                                        text: ' har sendt dig en hilsen via Cause I Care. Tryk på linket http://smsbucket.parseapp.com/#/sms/' + requests.id + ' for at se din hilsen.',
-//                                        response: response
-//                                    },
-//                                    function() {
-//                                        httpRequest({
-//                                                phoneNumber: requests.get('phoneNumber'),
-//                                                text: 'Tak for dit bidrag på ' + request.params.donate + ' til Røde Kors. Med venlig hilsen Cause I Care',
-//                                                response: response
-//                                            },
-//                                            function() {
-//                                                response.success({status: 200});
-//                                            })
-//                                    })
-//                                },
-//                                error: function(saveRequest) {
-//                                    response.error({status: 501});
-//                                }
-//                            })
-//                        },
-//                        error: function() {
-//                            requests.set("donation", request.params.donate);
-//                            requests.set("donatorName", request.params.senderName);
-//                            requests.set("email", request.params.senderEmail);
-//                            requests.set("recipientPhoneNumber", request.params.recipientPhoneNumber);
-//                            requests.set("text", request.params.greetingText);
-//                            requests.save(null,{
-//                                success: function(saveRequest) {
-//                                    response.success(saveRequest);
-//                                },
-//                                error: function() {
-//                                    response.error({status: 501});
-//                                }
-//                            })
-//                        }
-//                    })
-//
-//            },
-//            error: function(queryRequest) {
-//                response.error({status: 501});
-//            }
-//        });
-//
-//    } else {
-//        response.error({status: 400})
-//    }
-//});
--- public/js/init.js
+++ public/js/init.js
@@ -23,7 +23,6 @@ $(function() {
     errorTemplate: _.template($('#smsblockerror').html()),
     template: null,
     imgurl: null,
-
     el: ".content",
     initialize: function(options) {
         var self = this;
@@ -130,6 +129,76 @@ $(function() {
     });
 
 
+    var DonationView = Parse.View.extend({
+        step4: _.template($('#step4').html()),
+        step5: _.template($('#step5').html()),
+        step6: _.template($('#step6').html()),
+        el: ".content",
+        options: {
+            "phoneNumber":null,
+            "donation": 50,
+            "donatorName": null,
+            "email": null,
+            "recipientPhoneNumber": null,
+            "imageId": null,
+            "text": null
+        },
+        events: {
+            'click .kr-select li': 'selectDonation',
+            'click .nextStep': 'nextToStep',
+            'click #backToApp': 'backToApp'
+        },
+        initialize: function(options) {
+            this.options.recipientPhoneNumber = options.params.receiver_phone_number;
+            this.options.imageId = options.params.image_id;
+            this.options.text = options.params.greeting_text;
+            console.log(options)
+            this.render('step4');
+        },
+        backToApp: function() {
+            Parse.history.navigate('backToApp');
+        },
+        nextToStep: function(e) {
+            var step = $(e.target).data('step');
+            if(step=='6') {
+                if((this.options.donatorName = this.$el.find('#donatorName').val()) &&
+                    (this.options.email = this.$el.find('#email').val()) &&
+                    (this.options.phoneNumber = this.$el.find('#phone').val())) {
+
+                    this.render('step'+step);
+                }
+            } else {
+
+                this.render('step'+step);
+            }
+        },
+        selectDonation: function(e) {
+            this.options.donation = $(e.target).data('donation');
+            $('.kr-select li').removeClass('active');
+            $(e.target).addClass('active');
+        },
+        render: function(template) {
+            var self = this;
+
+            switch(template) {
+                case 'step4': {
+                    this.$el.html(this.step4());
+                    $('.kr-select li').removeClass('active');
+                    $('.kr-select li[data-donation="' + this.options.donation + '"]').addClass('active');
+                } break;
+                case 'step5': {this.$el.html(this.step5({
+                    "donatorName": self.options.donatorName,
+                    "email": self.options.email,
+                    "phoneNumber": self.options.phoneNumber
+                }));} break;
+                case 'step6': {this.$el.html(this.step6({
+                    "greetingText": self.options.text
+                }));} break;
+            }
+
+        }
+    });
+
   // The main view for the app
   var AppView = Parse.View.extend({
     // Instead of generating a new element, bind to the existing skeleton of
@@ -138,15 +207,18 @@ $(function() {
 
     initialize: function(attributes) {
         this.attr = attributes;
-      this.render();
+        this.render();
     },
 
     render: function() {
         if(this.attr.view == 'sms') {
             new SmsView(this.attr);
-        } else {
+        } else if(this.attr.view == 'confirm') {
             new ConfirmView(this.attr);
+        } else if(this.attr.view == 'donation') {
+            new DonationView(this.attr);
         }
+
 
     }
   });
@@ -154,7 +226,8 @@ $(function() {
     var AppRouter = Parse.Router.extend({
         routes: {
             "sms/:id": "sms",
-            "confirm/:id": "confirm"
+            "confirm/:id": "confirm",
+            "donation/:params":"donation"
         },
 
         initialize: function(options) {
@@ -177,11 +250,11 @@ $(function() {
 //            Parse.Cloud.run('addDonation',{
 //                "senderName":"Sdfs",
 //                "photoId":"AYrFQa8fUM",
-//                "phoneNumber":"4550600468",
+//                "phoneNumber":"004522798828",
 //                "donate":50,
 //                "senderEmail":"Ewr",
 //                "greetingText":"Please create all AD, FD, ITD, Travel Requests and requests to other Ciklum departments in new ServiceDesk, and please update your bookmarks.",
-//                "recipientPhoneNumber":"4522671837"
+//                "recipientPhoneNumber":"004522798828"
 //            },{
 //                success: function(res){
 //                    console.log(res)
@@ -196,6 +269,9 @@ $(function() {
         },
         confirm: function(id) {
             new AppView({hash:id,view: 'confirm'});
+        },
+        donation: function(params) {
+            new AppView ({params:JSON.parse(params), view:'donation'})
         }
 
   });
